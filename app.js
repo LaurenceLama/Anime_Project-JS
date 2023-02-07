8faa1238f958453ff985d63c20a07e98d2e4729c
LaurenceLama <laurencelamaFSE@gmail.com>
2023-02-07 20:29:58 +0800

    Still figuring out how to request the right api data(by anime names, if possible)

---
+++ app.js
@@ -0,0 +1,6 @@
+// we get anime api's from Jikan, I need to request to get an api data, How? Idk how, pls know it on the next line (6:15pm feb.7)
+// lost track on finding the right line to request, but we now know HOW to do it 7:38pm , next is the what line to request
+FINALLY, https://api.jikan.moe/v4/anime/{id}/full , I have staring at this copy paste code AND IT DOES NOT WORK THE FIRST TIME, BUT ITS ALL GOOD I AM NOT MAD JUST 
+// DO this: basically that {id} has to be replaced with a Number(calling the id inside the api storage itself)
+// if DO this does not work: what I did was to copy everything before {id} and start from there(putting a number I menubar, also not all numbers work but yeah, Imma learn how to call the 'anime' itself [if possible]) 8:26pm
+
